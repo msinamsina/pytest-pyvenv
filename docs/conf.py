0bdf78b7182655be4f18e8ea9f48eeb5548c3d9c
msina <msinamsina@gmail.com>
2024-02-25 00:36:20 +0330

    bump: version 0.1.7 → 0.1.8-b0

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, '../')
 project = 'pytest_env'
 copyright = '2024, Mohammad Sina Allahkaram'
 author = 'Mohammad Sina Allahkaram'
-release = '0.1.7'
+release = '0.1.8-b0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
